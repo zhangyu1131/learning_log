f2166021bcf6781ebd74c984c8a4c727a9e5f628
Rain <923446353@qq.com>
2018-01-17 17:11:53 +0800

    1.17 finish users and let users have their own data

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,39 +1,51 @@
 from django.shortcuts import render
 from .models import Topic, Entry
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,Http404
 from django.urls import reverse
 from .forms import TopicForm , EntryForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return render(request,'learning_logs/index.html')
 
+@login_required()#检查用户是否登录，仅当用户登录时才运行topics代码
 def topics(request):
-    topics=Topic.objects.order_by('date_added')
+    topics=Topic.objects.filter(owner=request.user).order_by('date_added')
     context={'topics':topics}
     return render(request,'learning_logs/topics.html',context)
 
+@login_required()
 def topic(request,topic_id):
     topic=Topic.objects.get(id=topic_id)
+    #确认请求的主题属于当前用户
+    if topic.owner!=request.user:
+        raise Http404
     entries=topic.entry_set.order_by('-date_added')
     context={'topic':topic,'entries':entries}
     return render(request,'learning_logs/topic.html',context)
 
+@login_required()
 def new_topic(request):
     if request.method!='POST':
         form=TopicForm()
     else:
         form=TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic=form.save(commit=False)
+            new_topic.owner=request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))
 
     context={'form':form}
     return render(request,'learning_logs/new_topic.html',context)
 
 
+@login_required()
 def new_entry(request,topic_id):
     topic=Topic.objects.get(id=topic_id)
+    if topic.owner!=request.user:
+        raise Http404
     if request.method!='POST':
         form=EntryForm()
     else:
@@ -47,10 +59,12 @@ def new_entry(request,topic_id):
     context={'topic':topic,'form':form}
     return render(request,'learning_logs/new_entry.html',context)
 
-
+@login_required()
 def edit_entry(request,entry_id):
     entry=Entry.objects.get(id=entry_id)
     topic=entry.topic
+    if topic.owner!=request.user:
+        raise Http404
 
     if request.method!='POST':
         form=EntryForm(instance=entry)
@@ -61,4 +75,4 @@ def edit_entry(request,entry_id):
             return HttpResponseRedirect(reverse('learning_logs:topic',args=[topic.id]))
 
     context={'entry':entry, 'topic':topic, 'form':form}
-    return render(request,'learning_logs/edit_entry.html',context                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  )
+    return render(request,'learning_logs/edit_entry.html',context)
--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,6 @@ from . import views
 app_name='users'
 urlpatterns=[
     url(r'^login/$',login,{'template_name':'users/login.html'},name='login'),
+    url(r'^logout/$',views.logout_view,name='logout'),
+    url(r'^register/$',views.register,name='register'),
 ]
